069a3cb3a8f6ce0bd15766cfc0c579a969c03ae5
Yehonatan Zecharia <yonti95@gmail.com>
2020-10-05 20:49:12 +0300

    Convert ts from str/bytes to int in receive/pop message

--- src/rsmq/cmd/pop_message.py
+++ src/rsmq/cmd/pop_message.py
@@ -29,4 +29,4 @@ class PopMessageCommand(BaseRSMQCommand):
         if not result:
             raise NoMessageInQueue(self.get_qname)
         [message_id, message, rc, ts] = result
-        return {'id': message_id, 'message': message, 'rc': rc, 'ts': ts}
+        return {'id': message_id, 'message': message, 'rc': rc, 'ts': int(ts)}
