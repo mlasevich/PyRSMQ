916146fc4693bf937382a81011762be93003b482
yehonatanz <yonti95@gmail.com>
2020-11-11 06:35:54 +0200

    Implement the transaction() contextmanager on deleteMessage (#10)
    * Implement the transaction() contextmanager on deleteMessage
    
    * Document DeleteMessage.get_transaction
    
    * Remove DeleteMessage.transaction context manager, to prevent multiple ways to do the same thing

--- src/rsmq/cmd/delete_message.py
+++ src/rsmq/cmd/delete_message.py
@@ -20,6 +20,15 @@ class DeleteMessageCommand(BaseRSMQCommand):
 
     def exec_command(self):
         ''' Exec Command '''
+        result = self.get_transaction().execute()
+
+        if int(result[0]) == 1 and int(result[1]) > 0:
+            return True
+
+        return False
+
+    def get_transaction(self):
+        ''' Returns a transaction (pipeline), pre-populated with the deleteMessage commands '''
         client = self.client
         queue_base = self.queue_base
         queue_key = self.queue_key
@@ -29,9 +38,4 @@ class DeleteMessageCommand(BaseRSMQCommand):
         tx.zrem(queue_base, message_id)
         tx.hdel(queue_key, message_id, "%s:rc" %
                 message_id, "%s:fr" % message_id)
-        result = tx.execute()
-
-        if int(result[0]) == 1 and int(result[1]) > 0:
-            return True
-
-        return False
+        return tx
